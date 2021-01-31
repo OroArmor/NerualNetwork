532580d63684a0626cdce0f89ecf24e12d222543
OroArmor <eliorona@live.com>
2021-01-31 15:52:49 -0800

    code clean up

--- src/test/java/com/oroarmor/neural_network/numberID/CreateLabelsFromFile.java
+++ src/test/java/com/oroarmor/neural_network/numberID/CreateLabelsFromFile.java
@@ -1,46 +1,47 @@
 package com.oroarmor.neural_network.numberID;
 
-import java.io.*;
-
-import processing.core.PApplet;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 
 public class CreateLabelsFromFile {
-	public static void main(String[] args) {
-		loadTextFromFile("numberID/test/",
-				"t10k-labels-idx1-ubyte-uncompressed", "C:\\oroarmor\\numberID\\test\\", "labels.txt", 10000);
-		loadTextFromFile("numberID/train/",
-				"train-labels-idx1-ubyte-uncompressed", "C:\\oroarmor\\numberID\\train\\", "labels.txt", 60000);
-		System.out.println("Done");
-	}
+    public static void main(String[] args) {
+        loadTextFromFile("numberID/test/",
+                "t10k-labels-idx1-ubyte-uncompressed", "C:\\oroarmor\\numberID\\test\\", "labels.txt", 10000);
+        loadTextFromFile("numberID/train/",
+                "train-labels-idx1-ubyte-uncompressed", "C:\\oroarmor\\numberID\\train\\", "labels.txt", 60000);
+        System.out.println("Done");
+    }
 
-	public static void loadTextFromFile(String filePath, String fileName, String savePath, String saveName, int amount) {
-		InputStream fos;
-		byte[] labelBytes = new byte[amount + 8];
+    public static void loadTextFromFile(String filePath, String fileName, String savePath, String saveName, int amount) {
+        InputStream fos;
+        byte[] labelBytes = new byte[amount + 8];
 
-		try {
-			fos = CreateLabelsFromFile.class.getClassLoader().getResourceAsStream(filePath + fileName);
-			if(fos == null) {
-				throw new NullPointerException("Could not find resource");
-			}
-			int read = fos.read(labelBytes);
-			fos.close();
-		} catch (Exception e) {
-			System.out.println(e.getLocalizedMessage());
-		}
-		try {
-			File f = new File(savePath + saveName);
-			boolean deleted = f.delete();
-			boolean created = f.createNewFile();
-			FileOutputStream saveOut = new FileOutputStream(f);
-			PrintStream stream = new PrintStream(saveOut);
-			for (int i = 8; i < labelBytes.length; i++) {
-				stream.print(labelBytes[i] + "");
-			}
+        try {
+            fos = CreateLabelsFromFile.class.getClassLoader().getResourceAsStream(filePath + fileName);
+            if (fos == null) {
+                throw new NullPointerException("Could not find resource");
+            }
+            int read = fos.read(labelBytes);
+            fos.close();
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+        try {
+            File f = new File(savePath + saveName);
+            boolean deleted = f.delete();
+            boolean created = f.createNewFile();
+            FileOutputStream saveOut = new FileOutputStream(f);
+            PrintStream stream = new PrintStream(saveOut);
+            for (int i = 8; i < labelBytes.length; i++) {
+                stream.print(labelBytes[i] + "");
+            }
 
-			stream.close();
-			saveOut.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            stream.close();
+            saveOut.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/test/java/com/oroarmor/neural_network/numberID/NumberIDNeuralNetwork.java
+++ src/test/java/com/oroarmor/neural_network/numberID/NumberIDNeuralNetwork.java
@@ -1,223 +1,224 @@
-package oroarmor.numberID;
+package com.oroarmor.neural_network.numberID;
 
 import java.io.File;
 import java.io.FileInputStream;
 
-import oroarmor.neuralnetwork.layer.FeedFowardLayer;
-import oroarmor.neuralnetwork.matrix.Matrix;
-import oroarmor.neuralnetwork.matrix.SoftMaxFunction;
-import oroarmor.neuralnetwork.network.NetworkSaver;
-import oroarmor.neuralnetwork.network.NeuralNetwork;
-import oroarmor.neuralnetwork.training.GetData;
-import oroarmor.neuralnetwork.training.Tester;
-import oroarmor.neuralnetwork.training.Trainer;
-import oroarmor.neuralnetwork.training.models.TotalError;
+import com.oroarmor.neural_network.layer.FeedForwardLayer;
+import com.oroarmor.neural_network.matrix.CPUMatrix;
+import com.oroarmor.neural_network.matrix.Matrix.MatrixType;
+import com.oroarmor.neural_network.matrix.function.SoftMaxFunction;
+import com.oroarmor.neural_network.network.NetworkSaver;
+import com.oroarmor.neural_network.network.NeuralNetwork;
+import com.oroarmor.neural_network.training.GetData;
+import com.oroarmor.neural_network.training.Tester;
+import com.oroarmor.neural_network.training.Trainer;
+import com.oroarmor.neural_network.training.models.TotalError;
 import processing.core.PApplet;
 import processing.core.PImage;
 
 public class NumberIDNeuralNetwork extends PApplet {
+    boolean reset = false;
+    NeuralNetwork<CPUMatrix> numberIDNetwork;
 
-	boolean reset = false;
-	NeuralNetwork numberIDNetwork;
-	PImage randomImage;
+    public static void main(String[] args) {
+        PApplet.main("com.oroarmor.neural_network.numberID.NumberIDNeuralNetwork");
+    }
 
-	public static void main(String[] args) {
-		PApplet.main("oroarmor.numberID.NumberIDNeuralNetwork");
-	}
+    public CPUMatrix getImageData(String string) {
+        return getImageData(loadImage(string));
+    }
 
-	public Matrix getImageData(String string) {
-		return getImageData(loadImage(string));
-	}
+    public CPUMatrix getImageData(PImage image) {
+        image.loadPixels();
+        double[] CPUMatrixArray = new double[image.height * image.width];
+        for (int i = 0; i < CPUMatrixArray.length; i++) {
+            CPUMatrixArray[i] = (double) brightness(image.pixels[i]) / 255f;
+        }
+        return new CPUMatrix(CPUMatrixArray, CPUMatrixArray.length, 1);
+    }
 
-	public Matrix getImageData(PImage image) {
-		image.loadPixels();
-		double[][] matrixArray = new double[image.height * image.width][1];
-		for (int i = 0; i < matrixArray.length; i++) {
-			matrixArray[i] = new double[] { (double) brightness(image.pixels[i]) / 255f };
-		}
-		return new Matrix(matrixArray);
-	}
+    @Override
+    public void settings() {
+        size(280, 400);
+    }
 
-	public void settings() {
-		size(280, 400);
-	}
+    @Override
+    public void setup() {
+        background(0);
+        noStroke();
+        numberIDNetwork = NetworkSaver.loadNetworkFromFile("C:\\oroarmor\\numberID\\", "numberIDNetwork.nn");
 
-	public void setup() {
-		background(0);
-		noStroke();
-		numberIDNetwork = NetworkSaver.loadNetworkFromFile("C:\\oroarmor\\numberID\\", "numberIDNetwork.nn");
+        if (numberIDNetwork == null || reset) {
+            numberIDNetwork = new NeuralNetwork<>(28 * 28);
+            numberIDNetwork.addLayer(new FeedForwardLayer<>(64, MatrixType.CPU));
+            numberIDNetwork.addLayer(new FeedForwardLayer<>(32, MatrixType.CPU));
+            numberIDNetwork.addLayer(new FeedForwardLayer<>(16, MatrixType.CPU));
+            numberIDNetwork.addLayer(new FeedForwardLayer<>(16, MatrixType.CPU));
+            numberIDNetwork.addLayer(new FeedForwardLayer<>(10, MatrixType.CPU));
+        }
+        test();
+        train();
+        test();
+    }
 
-		if (numberIDNetwork == null || reset) {
-			numberIDNetwork = new NeuralNetwork(28 * 28);
-			numberIDNetwork.addLayer(new FeedFowardLayer(64));
-			numberIDNetwork.addLayer(new FeedFowardLayer(32));
-			numberIDNetwork.addLayer(new FeedFowardLayer(16));
-			numberIDNetwork.addLayer(new FeedFowardLayer(16));
-			numberIDNetwork.addLayer(new FeedFowardLayer(10));
-		}
-//		test();
-		train(); 
-		test();
-//		noLoop();
-//		strokeWeight(28);
-	}
+    @Override
+    public void draw() {
+        if (mousePressed) {
+            stroke(255, 255 / 2f);
+            for (int i = 0; i < 5; i++) {
+                strokeWeight(25 - i * 5);
+                line(mouseX, mouseY, pmouseX, pmouseY);
+            }
+        }
+        fill(255);
+        noStroke();
+        rect(0, 280, 280, 120);
 
-	public void draw() {
-		if (mousePressed) {
-			stroke(255, 255 / 2);
-			for (int i = 0; i < 5; i++) {
-				strokeWeight(25 - i * 5);
-				line(mouseX, mouseY, pmouseX, pmouseY);
-			}
-		}
-		fill(255);
-		rect(0, 280, 280, 120);
+        PImage myTest = new PImage(280, 280);
+        myTest.loadPixels();
 
-		PImage imageFromScreen = new PImage(280, 280);
-		imageFromScreen.loadPixels();
+        myTest.copy(g.copy(), 0, 0, 280, 280, 0, 0, 280, 280);
 
-		imageFromScreen.copy(g.copy(), 0, 0, 280, 280, 0, 0, 280, 280);
+        myTest.resize(28, 28);
+        myTest.updatePixels();
 
-		imageFromScreen.resize(28, 28);
-		imageFromScreen.updatePixels();
+        CPUMatrix outputs = numberIDNetwork.feedFoward(getImageData(myTest)).applyFunction(new SoftMaxFunction());
 
-		Matrix outputs = numberIDNetwork.feedFoward(getImageData(imageFromScreen)).applyFunction(new SoftMaxFunction());
+        noStroke();
+        for (int i = 0; i < 10; i++) {
+            fill((float) (1 - outputs.getValue(i, 0)) * 255f, (float) outputs.getValue(i, 0) * 255f, 0);
+            rect(i * 280 / 10f, 280, 28, 120);
 
-		noStroke();
-		for (int i = 0; i < 10; i++) {
-			fill((float) (1 - outputs.getValue(i, 0)) * 255f, (float) (outputs.getValue(i, 0)) * 255f, 0);
-			rect(i * 280 / 10, 280, 28, 120);
-			fill(0);
-			text(i + ": \n" + outputs.getValue(i, 0), i * 28, 300);
-		}
-	}
+            fill(0);
+            text(i + ": \n" + outputs.getValue(i, 0), i * 28, 300);
+        }
+    }
 
-	public void keyPressed() {
-		if (key == 'c') {
-			background(0);
-		}
-	}
+    @Override
+    public void keyPressed() {
+        if (key == 'c') {
+            background(0);
+        }
+    }
 
-	public void train() {
-		long start = System.currentTimeMillis();
-		int numImages = 60000;
-		
-		
-		
-		for (int repeats = 0; repeats < 5; repeats++) {
-			int threads = 8;
-			Thread[] trainingThreads = new Thread[threads];
+    public void train() {
+        long start = System.currentTimeMillis();
+        for (int repeats = 0; repeats < 1; repeats++) {
+            int threads = 12;
+            Thread[] trainingThreads = new Thread[threads];
 
-			
+            int numImages = 60000;
 
-			for (int i = 0; i < threads; i++) {
+            for (int i = 0; i < threads; i++) {
+                GetData<CPUMatrix> getInputs = new GetData<CPUMatrix>(
+                        new String[]{i + "", numImages / threads + ""}) {
+                    @Override
+                    public CPUMatrix getData(String[] args) {
+                        return getImageData(loadImage("C:\\oroarmor\\numberID\\train\\images\\"
+                                + (Integer.parseInt(globalArgs[0]) * Integer.parseInt(globalArgs[1])
+                                + Integer.parseInt(args[0]))
+                                + ".png"));
+                    }
+                };
 
-				GetData getInputs = new GetData(new String[] { i + "", numImages / threads + "" }) {
-					public Matrix getData(String[] args) {
-						Matrix images = getImageData(loadImage("C:\\oroarmor\\numberID\\train\\images\\"
-								+ (Integer.parseInt(globalArgs[0]) * Integer.parseInt(globalArgs[1])
-										+ Integer.parseInt(args[0]))
-								+ ".png"));
-						return images;
-					}
-				};
+                GetData<CPUMatrix> getOutputs = new GetData<CPUMatrix>(
+                        new String[]{i + "", numImages / threads + ""}) {
+                    @Override
+                    public CPUMatrix getData(String[] args) {
+                        Character trainValue = getIndex("C:\\oroarmor\\numberID\\train\\labels.txt",
+                                Integer.parseInt(globalArgs[0]) * Integer.parseInt(globalArgs[1])
+                                        + Integer.parseInt(args[0]));
+                        CPUMatrix output = new CPUMatrix(10, 1);
+                        output.setValue(Integer.parseInt(trainValue + ""), 0, 1);
+                        return output;
+                    }
+                };
 
-				GetData getOutputs = new GetData(new String[] { i + "", numImages / threads + "" }) {
-					public Matrix getData(String[] args) {
-						Character trainValue = getIndex("C:\\oroarmor\\numberID\\train\\labels.txt",
-								Integer.parseInt(globalArgs[0]) * Integer.parseInt(globalArgs[1])
-										+ Integer.parseInt(args[0]));
-						Matrix output = new Matrix(10, 1);
-						output.setValue(Integer.parseInt(trainValue + ""), 0, 1);
-						return output;
-					}
-				};
-				Trainer trainer = new Trainer(getInputs, getOutputs, numberIDNetwork, new TotalError(0.01));
-				Thread thread = new Thread(trainer);
-				trainingThreads[i] = thread;
-			}
-			for (Thread thread : trainingThreads) {
-				thread.start();
-			}
+                Trainer<CPUMatrix> trainer = new Trainer<>(getInputs, getOutputs, numberIDNetwork,
+                        new TotalError(0.01));
 
-			try {
-				for (Thread thread : trainingThreads) {
-					thread.join();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			System.out.println(repeats + " " + ((System.currentTimeMillis() - start) / (1000f * (repeats + 1))));
-		}
-		System.out.println(numberIDNetwork.getTrainingAttemps());
-		System.out.println((System.currentTimeMillis() - start) / 1000f + " total seconds");
-		NetworkSaver.saveNetworkToFile(numberIDNetwork, "numberIDNetwork.nn", "C:\\oroarmor\\numberID\\");
-	}
+                Thread thread = new Thread(trainer);
+                trainingThreads[i] = thread;
+            }
+            for (Thread thread : trainingThreads) {
+                thread.start();
+            }
 
-	public void test() {
-		Tester.numCorrect = 0;
-		int threads = 8;
-		Thread[] testThreads = new Thread[threads];
+            try {
+                for (Thread thread : trainingThreads) {
+                    thread.join();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println(repeats + " " + (System.currentTimeMillis() - start) / (1000f * (repeats + 1)));
+        }
+        System.out.println(numberIDNetwork.getTrainingAttemps());
+        System.out.println((System.currentTimeMillis() - start) / 1000f + " total seconds");
+        NetworkSaver.saveNetworkToFile(numberIDNetwork, "numberIDNetwork.nn", "C:\\oroarmor\\numberID\\");
+    }
 
-		int numImages = 10000;
+    public void test() {
+        Tester.numCorrect = 0;
+        int threads = 8;
+        Thread[] testThreads = new Thread[threads];
 
-		for (int i = 0; i < threads; i++) {
+        int numImages = 10000;
 
-			GetData getInputs = new GetData(new String[] { i + "", numImages / threads + "" }) {
-				public Matrix getData(String[] args) {
-					Matrix images = getImageData(loadImage("C:\\oroarmor\\numberID\\test\\images\\"
-							+ (Integer.parseInt(globalArgs[0]) * Integer.parseInt(globalArgs[1])
-									+ Integer.parseInt(args[0]))
-							+ ".png"));
-					return images;
-				}
-			};
+        for (int i = 0; i < threads; i++) {
 
-			GetData getOutputs = new GetData(new String[] { i + "", numImages / threads + "" }) {
-				public Matrix getData(String[] args) {
-					Character trainValue = getIndex("C:\\oroarmor\\numberID\\test\\labels.txt",
-							Integer.parseInt(globalArgs[0]) * Integer.parseInt(globalArgs[1])
-									+ Integer.parseInt(args[0]));
-					Matrix output = new Matrix(10, 1);
-					output.setValue(Integer.parseInt(trainValue + ""), 0, 1);
-					return output;
-				}
-			};
+            GetData<CPUMatrix> getInputs = new GetData<CPUMatrix>(new String[]{i + "", numImages / threads + ""}) {
+                @Override
+                public CPUMatrix getData(String[] args) {
+                    return getImageData(loadImage("C:\\oroarmor\\numberID\\test\\images\\"
+                            + (Integer.parseInt(globalArgs[0]) * Integer.parseInt(globalArgs[1])
+                            + Integer.parseInt(args[0]))
+                            + ".png"));
+                }
+            };
 
-			Tester tester = new Tester(getInputs, getOutputs, numberIDNetwork);
+            GetData<CPUMatrix> getOutputs = new GetData<CPUMatrix>(new String[]{i + "", numImages / threads + ""}) {
+                @Override
+                public CPUMatrix getData(String[] args) {
+                    Character trainValue = getIndex("C:\\.oroarmor\\numberID\\test\\labels.txt",
+                            Integer.parseInt(globalArgs[0]) * Integer.parseInt(globalArgs[1])
+                                    + Integer.parseInt(args[0]));
+                    CPUMatrix output = new CPUMatrix(10, 1);
+                    output.setValue(Integer.parseInt(trainValue + ""), 0, 1);
+                    return output;
+                }
+            };
 
-			Thread thread = new Thread(tester);
-			testThreads[i] = thread;
-		}
-		for (Thread thread : testThreads) {
-			thread.start();
-		}
+            Tester<CPUMatrix> tester = new Tester<>(getInputs, getOutputs, numberIDNetwork);
 
-		try {
-			for (Thread thread : testThreads) {
-				thread.join();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println(Tester.numCorrect);
-	}
+            Thread thread = new Thread(tester);
+            testThreads[i] = thread;
+        }
+        for (Thread thread : testThreads) {
+            thread.start();
+        }
 
-	public Character getIndex(String textFilePath, int index) {
-		FileInputStream textFile = null;
-		try {
-			textFile = new FileInputStream(new File(textFilePath));
-			textFile.skip(index);
-			Character value = (char) textFile.read();
-			textFile.close();
-			return value;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+        try {
+            for (Thread thread : testThreads) {
+                thread.join();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(Tester.numCorrect);
+    }
 
-//	public void mouseClicked() {
-//		setup();
-//	}
+    public Character getIndex(String textFilePath, int index) {
+        FileInputStream textFile;
+        try {
+            textFile = new FileInputStream(new File(textFilePath));
+            long skip = textFile.skip(index);
+            Character value = (char) textFile.read();
+            textFile.close();
+            return value;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
