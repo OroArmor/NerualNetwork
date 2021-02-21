ce72d16781f9874523f6e77b92975bfdda882d5c
Eli Orona <eliorona@live.com>
2021-02-20 21:40:27 -0800

    Working GPG Signing

--- .build/deploy.sh
+++ .build/deploy.sh
@@ -2,9 +2,9 @@ set -euo pipefail
 IFS=$'\n\t'
 
 function cleanup {
-    echo "🧹 Cleanup..."
-    rm -f gradle.properties golo-dev-sign.asc
-}
+	    echo "🧹 Cleanup..."
+	        rm -f gradle.properties eliorona-sign.asc
+	}
 
 trap cleanup SIGINT SIGTERM ERR EXIT
 
@@ -13,15 +13,15 @@ echo "🚀 Preparing to deploy..."
 echo "🔑 Decrypting files..."
 
 gpg --quiet --batch --yes --decrypt --passphrase="${GPG_SECRET}" \
-    --output golo-dev-sign.asc .build/eliorona-sign.asc.gpg
+	    --output eliorona-sign.asc .build/eliorona-sign.asc.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="${GPG_SECRET}" \
-    --output gradle.properties .build/gradle.properties.gpg
+	    --output gradle.properties .build/gradle.properties.gpg
 
-gpg --fast-import --no-tty --batch --yes golo-dev-sign.asc
+gpg --fast-import --no-tty --batch --yes eliorona-sign.asc
 
 echo "📦 Publishing..."
 
-./gradlew publish
+./gradlew uploadArchives
 
 echo "✅ Done!"
